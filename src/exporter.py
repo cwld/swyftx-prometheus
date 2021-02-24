daf940c985caf9ea5c369db474176ef8e8ff115a
colin liddle <colin.liddle@innablr.com.au>
2021-02-24 15:07:38 +1100

    Add better logging

--- src/exporter.py
+++ src/exporter.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
 
   numeric_level = getattr(logging, args.log_level.upper(), None)
   logging.basicConfig(level=numeric_level)
-  logging.info("Exporter starting..")
+  logging.info("Retrieving initial metrics..")
 
   rate_in_seconds = convert_to_seconds(args.rate)
 
@@ -48,12 +48,14 @@ if __name__ == '__main__':
   # Populate initial metrics
   populate_metrics()
 
+  logging.info("Starting server...")
   start_http_server(args.port)
 
   sleep_time = rate_in_seconds
   while True:
     time.sleep(sleep_time)
     time_before_populate = time.time()
+    logging.info("Refreshing metrics...")
     populate_metrics()
     time_after_populate = time.time()
 
